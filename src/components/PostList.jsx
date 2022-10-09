f0f381066fa89a8111497b82c49ebae7fefd759d
omar2004712 <o.bahutair@gmail.com>
2022-10-09 19:49:46 +0400

    build(post-list): built the PostList component

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,6 +3,7 @@
 import '../styles/index.css';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import profilePhoto from '../images/profile-photo.png';
 import { fetchPosts } from '../actions';
 
 function PostList({ posts, fetchPosts }) {
@@ -10,9 +11,22 @@ function PostList({ posts, fetchPosts }) {
     fetchPosts();
   }, []);
 
-  console.log(posts);
+  const renderList = () =>
+    // eslint-disable-next-line implicit-arrow-linebreak
+    posts.map((post) => (
+      <div
+        className="flex p-4 rounded bg-gray-50 items-center gap-3 shadow-md"
+        key={post.id}
+      >
+        <img src={profilePhoto} alt="" className="w-12 h-12 rounded-full" />
+        <div>
+          <h2 className="text-xl font-semibold">{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      </div>
+    ));
 
-  return <div>PostList</div>;
+  return <div className="flex flex-col gap-2 px-6 py-3">{renderList()}</div>;
 }
 
 const mapStateToProps = (state) => ({ posts: state.posts });
