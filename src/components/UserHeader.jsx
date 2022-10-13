ad5a568cfb0018b3907d7c847ed9564d842b9e7d
omar2004712 <o.bahutair@gmail.com>
2022-10-13 08:34:46 +0400

    refactor(user-header): refactored the filtering of user to be in the mapStateToProps function

--- src/components/UserHeader.jsx
+++ src/components/UserHeader.jsx
@@ -4,12 +4,20 @@ import { connect } from 'react-redux';
 import { fetchUser } from '../actions';
 
 // eslint-disable-next-line no-shadow, react/prop-types
-function UserHeader({ userId, fetchUser }) {
+function UserHeader({ user, userId, fetchUser }) {
   useEffect(() => {
     fetchUser(userId);
   }, []);
 
-  return <div>User Header</div>;
+  if (!user) {
+    return null;
+  }
+
+  return <div className="font-semibold text-gray-400">{user.name}</div>;
 }
 
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = (state, ownProps) => ({
+  user: state.users.find((user) => user.id === ownProps.userId),
+});
+
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
